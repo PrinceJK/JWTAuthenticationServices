3c810eda2fc3ad7e2600f5c3cc5e27ecf6a80fff
John Kehinde <kehindejohn018@gmail.com>
2021-10-17 00:51:52 +0100

    Added user entity

--- JWTAuthenticationServices/Entities/RefreshToken.cs
+++ JWTAuthenticationServices/Entities/RefreshToken.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace JWTAuthenticationServices.Entities
 {
