faae3f16155f6098bb325dfc3a2c88c6b79e5585
John Kehinde <kehindejohn018@gmail.com>
2021-10-17 01:37:27 +0100

    Completed token services

--- JWTAuthenticationServices/Startup.cs
+++ JWTAuthenticationServices/Startup.cs
@@ -1,26 +1,42 @@
+using JWTAuthenticationServices.Authorization;
+using JWTAuthenticationServices.Entities;
+using JWTAuthenticationServices.Helpers;
+using JWTAuthenticationServices.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using BCryptNet = BCrypt.Net.BCrypt;
 
 namespace JWTAuthenticationServices
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>();
+            services.AddCors();
+            services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.IgnoreNullValues = true);
 
-            services.AddControllers();
+            //services.AddControllers();
+
+            // configure strongly typed settings object
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            // configure DI for application services
+            services.AddScoped<IJwtUtils, JwtUtils>();
+            services.AddScoped<IUserService, UserService>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWTAuthenticationServices", Version = "v1" });
@@ -28,8 +44,9 @@ namespace JWTAuthenticationServices
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
+            createTestUser(context);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,12 +58,39 @@ namespace JWTAuthenticationServices
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // global cors policy
+            app.UseCors(x => x
+                .SetIsOriginAllowed(origin => true)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+
+            // global error handler
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+
+            // custom jwt auth middleware
+            app.UseMiddleware<JwtMiddleware>();
+
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void createTestUser(DataContext context)
+        {
+            // add hardcoded test user to db on startup
+            var testUser = new User
+            {
+                FirstName = "Test",
+                LastName = "User",
+                Username = "test",
+                PasswordHash = BCryptNet.HashPassword("test")
+            };
+            context.Users.Add(testUser);
+            context.SaveChanges();
         }
     }
 }
